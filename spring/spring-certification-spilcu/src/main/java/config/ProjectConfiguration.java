18933b94813d0f2bbb2ab1f6e7b413b0295b3abe
trobertz <thomas.robertz@bcmsolutions.de>
2021-02-27 20:06:09 +0100

    Resolved ambiguous bean declaration with @Primary

--- spring/spring-certification-spilcu/src/main/java/config/ProjectConfiguration.java
+++ spring/spring-certification-spilcu/src/main/java/config/ProjectConfiguration.java
@@ -3,12 +3,14 @@ package config;
 import beans.MyBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class ProjectConfiguration {
 
     // By using this annotation, the INSTANCE of the bean will be automatically placed into the spring context.
     @Bean
+    @Primary
     public MyBean myBean() { // Usually we would put  a verb in the name, like "getMyBean" but this is a special case, we just use the bean name.
         MyBean b = new MyBean();
         b.setText("Hello");
