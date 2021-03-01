f0358aa7576d00219aeeab9125f7f7fe204a2d21
trobertz <thomas.robertz@bcmsolutions.de>
2021-03-01 16:28:46 +0100

    Removed beans and added services and repositories

--- spring/spring-certification-spilcu/src/main/java/main/Main.java
+++ spring/spring-certification-spilcu/src/main/java/main/Main.java
@@ -1,14 +1,11 @@
 package main;
 
-import beans.MyBean;
 import config.ProjectConfiguration;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.Arrays;
 
 public class Main {
-
-    MyBean myBean;
 
     public static void main(String[] args) {
         Main main = new Main();
@@ -18,16 +15,9 @@ public class Main {
         try (AnnotationConfigApplicationContext context =
                      new AnnotationConfigApplicationContext(ProjectConfiguration.class)) {
 
-            // Get a MyBean instance from the context
-            myBean = context.getBean(MyBean.class);
-
-            System.out.println(myBean.getText());
-
-            System.out.println(myBean);
-
             System.out.println("Registered beans: ");
             Arrays.asList(context.getBeanDefinitionNames()).forEach(b ->
-                    System.out.println(b)
+                    System.out.printf("Bean: %s\n", b)
             );
         }
     }
