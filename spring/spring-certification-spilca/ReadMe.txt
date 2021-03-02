b0ac22af97a1b788b333f1a5bf4bdcda0d54fe6c
trobertz <thomas.robertz@bcmsolutions.de>
2021-03-02 18:40:42 +0100

    Converted to multi module project, each lesson is a module

--- spring/spring-certification-spilca/ReadMe.txt
+++ spring/spring-certification-spilca/ReadMe.txt
@@ -3,3 +3,6 @@ Spring certification Spilca
 
 Follow-along code for the Spring certification tutorial by Laurentiu Spilca on youtube:
 https://www.youtube.com/watch?v=m-L-r862J-E&t=1126s
+
+Lessons 1 and 2: Context, beans, wiring
+Lesson 3: Database
--- spring/spring-certification-spilca/lesson-1-2/src/main/java/config/ProjectConfiguration.java
+++ spring/spring-certification-spilca/lesson-1-2/src/main/java/config/ProjectConfiguration.java
@@ -1,7 +1,5 @@
 package config;
 
-import beans.Cat;
-import beans.Owner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
