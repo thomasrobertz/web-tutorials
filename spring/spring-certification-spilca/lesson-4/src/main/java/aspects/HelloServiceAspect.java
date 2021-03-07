181653cc5db24c4d5d9830fc4c15685b86151bc2
trobertz <thomas.robertz@bcmsolutions.de>
2021-03-07 14:05:18 +0100

    Added AOP infrastructure

---
+++ spring/spring-certification-spilca/lesson-4/src/main/java/aspects/HelloServiceAspect.java
@@ -0,0 +1,10 @@
+package aspects;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class HelloServiceAspect {
+
+}
--- spring/spring-certification-spilca/lesson-4/src/main/java/main/Main.java
+++ spring/spring-certification-spilca/lesson-4/src/main/java/main/Main.java
@@ -4,12 +4,18 @@ import config.ProjectConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import services.HelloService;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         try(AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class)) {
             HelloService hs = context.getBean(HelloService.class);
 
             hs.sayHelloTo("Paul");
+
+            Arrays.asList(context.getBeanDefinitionNames()).forEach(b ->
+                    System.out.printf("Bean: %s\n", b)
+            );
         }
     }
 }
