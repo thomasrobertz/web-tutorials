a44bdc8e076d4cfa3f5a3461e39450846e80cd47
trobertz <thomas.robertz@bcmsolutions.de>
2021-03-07 14:34:31 +0100

    @AfterThrowing advice - not throwing anything yet

--- spring/spring-certification-spilca/lesson-4/src/main/java/aspects/HelloServiceAspect.java
+++ spring/spring-certification-spilca/lesson-4/src/main/java/aspects/HelloServiceAspect.java
@@ -1,14 +1,29 @@
 package aspects;
 
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class HelloServiceAspect {
+
     @Before("execution (* services.HelloService.sayHelloTo(..))")
-    public void before() {
+    public void beforeSayHelloTo() {
         System.out.println("Before services.HelloService.sayHelloTo()");
+    }
+
+    @After("execution (* services.HelloService.sayHelloTo(..))")
+    public void afterSayHelloTo() {
+        System.out.println("After services.HelloService.sayHelloTo()");
+    }
+
+    @AfterReturning("execution (* services.HelloService.sayHelloTo(..))")
+    public void afterReturningSayHelloTo() {
+        System.out.println("AfterReturning services.HelloService.sayHelloTo()");
+    }
+
+    @AfterThrowing("execution (* services.HelloService.sayHelloTo(..))")
+    public void afterThrowingSayHelloTo() {
+        System.out.println("AfterThrowing services.HelloService.sayHelloTo()");
     }
 }
