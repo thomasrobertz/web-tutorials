4ad34433d144b67c7d3cb812170fa0630bd37be9
trobertz <thomas.robertz@bcmsolutions.de>
2021-03-11 19:15:36 +0100

    Return string with @ResponseBody

---
+++ spring/spring-certification-spilca/lesson-8/src/main/java/com/laurntiuspilca/springboot101/controllers/HelloController.java
@@ -0,0 +1,14 @@
+package com.laurntiuspilca.springboot101.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "Hello World!";
+    }
+}
