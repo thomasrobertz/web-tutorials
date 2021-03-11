16f3f38c940b74de4346febb3b61f6bb83daa9b2
trobertz <thomas.robertz@bcmsolutions.de>
2021-03-11 19:18:44 +0100

    @RestController instead of @Controller and @ResponseBody

--- spring/spring-certification-spilca/lesson-8/src/main/java/com/laurntiuspilca/springboot101/controllers/HelloController.java
+++ spring/spring-certification-spilca/lesson-8/src/main/java/com/laurntiuspilca/springboot101/controllers/HelloController.java
@@ -3,11 +3,11 @@ package com.laurntiuspilca.springboot101.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
     @RequestMapping("/hello")
-    @ResponseBody
     public String hello() {
         return "Hello World!";
     }
