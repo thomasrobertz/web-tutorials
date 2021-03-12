6b872c39211b2a5a05b0d652c2ca0edeea06635e
trobertz <thomas.robertz@bcmsolutions.de>
2021-03-12 20:17:49 +0100

    Refactor lesson names

---
+++ spring/spring-certification-spilca/lesson-9/src/test/java/com/laurntiuspilca/springbootrest/SpringbootRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laurntiuspilca.springbootrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
