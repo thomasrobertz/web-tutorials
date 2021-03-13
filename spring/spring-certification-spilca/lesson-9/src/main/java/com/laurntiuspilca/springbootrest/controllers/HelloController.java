139dadb5e62720b766fa385eb405154973a44d20
trobertz <thomas.robertz@bcmsolutions.de>
2021-03-13 20:04:39 +0100

    PostMapping with RequestBody

--- spring/spring-certification-spilca/lesson-9/src/main/java/com/laurntiuspilca/springbootrest/controllers/HelloController.java
+++ spring/spring-certification-spilca/lesson-9/src/main/java/com/laurntiuspilca/springbootrest/controllers/HelloController.java
@@ -1,14 +1,18 @@
 package com.laurntiuspilca.springbootrest.controllers;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.laurntiuspilca.springbootrest.dto.Person;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
-    @RequestMapping(method = RequestMethod.GET, path = "/hello/{myname}")
+
+    @GetMapping(path = "/hello/{myname}")
     public String hello(@PathVariable("myname") String name) {
         return "Hello " + name;
+    }
+
+    @PostMapping(path = "/goodbye")
+    public String goodbye(@RequestBody Person person) {
+        return "Goodbye, " + person.getName();
     }
 }
---
+++ spring/spring-certification-spilca/lesson-9/src/main/java/com/laurntiuspilca/springbootrest/dto/Person.java
@@ -0,0 +1,13 @@
+package com.laurntiuspilca.springbootrest.dto;
+
+public class Person {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
