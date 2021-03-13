73ff13583aff01021c454e9677dd89d3eb7171c5
trobertz <thomas.robertz@bcmsolutions.de>
2021-03-13 20:26:13 +0100

    Return objects and lists

--- spring/spring-certification-spilca/lesson-9/src/main/java/com/laurntiuspilca/springbootrest/controllers/HelloController.java
+++ spring/spring-certification-spilca/lesson-9/src/main/java/com/laurntiuspilca/springbootrest/controllers/HelloController.java
@@ -3,6 +3,9 @@ package com.laurntiuspilca.springbootrest.controllers;
 import com.laurntiuspilca.springbootrest.dto.Person;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 public class HelloController {
 
@@ -14,5 +17,30 @@ public class HelloController {
     @PostMapping(path = "/goodbye")
     public String goodbye(@RequestBody Person person) {
         return "Goodbye, " + person.getName();
+    }
+
+    @GetMapping(path = "/person")
+    public Person getPerson() {
+        Person p = new Person();
+        p.setName("peter");
+        return p;
+    }
+
+    @GetMapping(path = "/persons")
+    public Person[] getPersons() {
+        Person p1 = new Person();
+        p1.setName("john");
+        Person p2 = new Person();
+        p2.setName("peter");
+        return new Person[] {p1, p2};
+    }
+
+    @GetMapping(path = "/personslist")
+    public List<Person> getPersonsAsList() {
+        Person p1 = new Person();
+        p1.setName("john");
+        Person p2 = new Person();
+        p2.setName("peter");
+        return Arrays.asList(p1, p2);
     }
 }
