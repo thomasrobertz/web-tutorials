7fd55d322dae807d2856cc1d9eae67328008fc6e
trobertz <thomas.robertz@bcmsolutions.de>
2021-03-21 20:09:35 +0100

    Add response header

--- spring/spring-certification-spilca/lesson-10/src/main/java/com/spilca/simpleweb/controllers/HelloController.java
+++ spring/spring-certification-spilca/lesson-10/src/main/java/com/spilca/simpleweb/controllers/HelloController.java
@@ -1,13 +1,24 @@
 package com.spilca.simpleweb.controllers;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+
 @RestController
 public class HelloController {
+
     @GetMapping(path = "/test")
-    public String test(@RequestHeader String a, @RequestHeader String b, @RequestHeader String c) {
+    public String test(@RequestHeader String a, @RequestHeader String b, @RequestHeader String c, HttpServletResponse response) {
+        response.addHeader("cutsomResponseHeader", "HelloWorld");
         return String.join(" ", a, b, c);
+    }
+
+    @GetMapping(path = "/all")
+    public Map<String, String> all(@RequestHeader Map<String, String> requestParameterMap) {
+        return requestParameterMap;
     }
 }
