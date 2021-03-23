6d86fb142741072a9d42fa5ad0fd73c62484ae11
trobertz <thomas.robertz@bcmsolutions.de>
2021-03-23 18:19:09 +0100

    Paging and Sorting

--- spring/spring-certification-spilca/lesson-11/src/main/java/com/laurentiuspilca/springdata/controllers/ProductController.java
+++ spring/spring-certification-spilca/lesson-11/src/main/java/com/laurentiuspilca/springdata/controllers/ProductController.java
@@ -4,9 +4,13 @@ import com.laurentiuspilca.springdata.entities.Product;
 import com.laurentiuspilca.springdata.repositories.ProductPagedRepository;
 import com.laurentiuspilca.springdata.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/product")
@@ -28,5 +32,10 @@ public class ProductController {
     @GetMapping(path = "/sort")
     public Iterable<Product> getAllSortedDescendingById() {
         return repository.findAll(Sort.by("id").descending());
+    }
+
+    @GetMapping(path = "/page/{page}")
+    public List<Product> getProductByPage(@PathVariable int page) {
+        return repository.findAll(PageRequest.of(page, 2, Sort.by("price").ascending())).toList();
     }
 }
