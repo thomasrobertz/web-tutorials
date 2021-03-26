41263856b3f822157ee41846717aaf5f14961ba2
trobertz <thomas.robertz@bcmsolutions.de>
2021-03-26 17:39:28 +0100

    Proxied request scoped service

--- spring/spring-certification-spilca/lesson-14/src/main/java/com/laurentiuspilca/webscopes/services/RandomNumberService.java
+++ spring/spring-certification-spilca/lesson-14/src/main/java/com/laurentiuspilca/webscopes/services/RandomNumberService.java
@@ -1,13 +1,19 @@
 package com.laurentiuspilca.webscopes.services;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.WebApplicationContext;
+
 import java.util.Random;
 
+@Service
+@Scope(value = WebApplicationContext.SCOPE_REQUEST, proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class RandomNumberService {
     private final int randomNumber;
     public RandomNumberService() {
         randomNumber = new Random().nextInt(1000);
     }
-
     public int getRandomNumber() {
         return randomNumber;
     }
