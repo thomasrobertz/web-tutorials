ac7ac81e4ea297efb774fb0ca28f748487a95475
trobertz <thomas.robertz@bcmsolutions.de>
2021-05-08 10:48:43 +0200

    Inline template

--- angular/hello-world/src/app/app.component.ts
+++ angular/hello-world/src/app/app.component.ts
@@ -2,7 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <h2>
+      Welcome!
+    </h2>
+  `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
