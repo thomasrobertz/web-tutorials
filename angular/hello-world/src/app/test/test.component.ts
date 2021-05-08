8abc072757fd0b992092efb2fb26085c45a47d8b
trobertz <thomas.robertz@bcmsolutions.de>
2021-05-08 19:29:49 +0200

    ngIf

--- angular/hello-world/src/app/test/test.component.ts
+++ angular/hello-world/src/app/test/test.component.ts
@@ -3,16 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   template: `
-    <h2>
+    <h2 *ngIf="displayWelcomeMessage; else myElseBlock">
       Welcome, {{name}}
     </h2> 
-    <input [(ngModel)]="name" type="text" /> 
+
+    <ng-template #myElseBlock>
+      <h2>
+        Welcome message is hidden.
+      </h2>
+    </ng-template>
   `,
   styles: []
 })
 export class TestComponent implements OnInit {
 
-  public name = "";
+  public name = "Frank";
+  public displayWelcomeMessage = false;
 
   constructor() { }
 
