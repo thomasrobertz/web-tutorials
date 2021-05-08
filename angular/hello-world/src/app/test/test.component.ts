8b22bef3f3aac55aed84391a678fd67a1fa92637
trobertz <thomas.robertz@bcmsolutions.de>
2021-05-08 19:34:08 +0200

    concise ngIf - else

--- angular/hello-world/src/app/test/test.component.ts
+++ angular/hello-world/src/app/test/test.component.ts
@@ -3,15 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   template: `
-    <h2 *ngIf="displayWelcomeMessage; else myElseBlock">
-      Welcome, {{name}}
-    </h2> 
+    
+  <h2 *ngIf="displayWelcomeMessage; then affirmative; else negative"></h2> 
 
-    <ng-template #myElseBlock>
-      <h2>
-        Welcome message is hidden.
-      </h2>
-    </ng-template>
+  <ng-template #affirmative>
+    <h2>
+      Welcome, {{name}}
+    </h2>
+  </ng-template>
+
+  <ng-template #negative>
+    <h2>
+      Welcome message is hidden.
+    </h2>
+  </ng-template>
   `,
   styles: []
 })
