1c53ccc7f6f43506aa66625f37eba09cd1913b2b
trobertz <thomas.robertz@bcmsolutions.de>
2021-05-08 10:59:23 +0200

    Interpolation

--- angular/hello-world/src/app/app.component.ts
+++ angular/hello-world/src/app/app.component.ts
@@ -2,11 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <h2>
-      Welcome!
-    </h2>
-  `,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
--- angular/hello-world/src/app/test/test.component.ts
+++ angular/hello-world/src/app/test/test.component.ts
@@ -2,10 +2,16 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  templateUrl: './test.component.html',
+  template: `
+    <h2>
+      test.component.ts {{message}}
+    </h2>
+  `,
   styleUrls: ['./test.component.scss']
 })
 export class TestComponent implements OnInit {
+
+  public message = "Hello!";
 
   constructor() { }
 
