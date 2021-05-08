2d465acc9f5ae09aa65ebfbcb5d7ed73ecd65f5d
trobertz <thomas.robertz@bcmsolutions.de>
2021-05-08 11:09:49 +0200

    More interpolation

--- angular/hello-world/src/app/test/test.component.ts
+++ angular/hello-world/src/app/test/test.component.ts
@@ -4,18 +4,28 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-test',
   template: `
     <h2>
-      test.component.ts {{message}}
+      test.component.ts {{ message }}<br />
+      {{ message.length + 1 }}<br />
+      {{ "UC: " + message.toUpperCase() }}<br />
+      {{ greetAndShowMessage() }}<br />
+      {{ siteUrl }}
     </h2>
+    <hr />
   `,
   styleUrls: ['./test.component.scss']
 })
 export class TestComponent implements OnInit {
 
   public message = "Hello!";
+  public siteUrl = window.location.href;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  greetAndShowMessage() {
+    return "Greetings, here is your message: " + this.message;
   }
 
 }
