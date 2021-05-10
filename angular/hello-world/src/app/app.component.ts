2bb5eb40cc177fff96868925f95a1a6b29534474
trobertz <thomas.robertz@bcmsolutions.de>
2021-05-10 18:52:27 +0200

    Pipes examples

--- angular/hello-world/src/app/app.component.ts
+++ angular/hello-world/src/app/app.component.ts
@@ -7,6 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hello-world';
-  public name = "From AppComponent";
-  public childMessage;
 }
--- angular/hello-world/src/app/test/test.component.ts
+++ angular/hello-world/src/app/test/test.component.ts
@@ -3,23 +3,26 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-test',
   template: `  
-    <h2>{{ "This is Input from somewhere above: " + parentData }}</h2>
-    <button (click)="fireEvent()">Send to parent</button>
+    <h2>{{ name | lowercase }}</h2>
+    <h2>{{ name | slice:2:4 }}</h2>
+    <h2>{{ person | json }}</h2>
+    <h2>{{ 5.6789 | number:'3.1-4' }}</h2>
+    <h2>{{ date | date:'short'  }}</h2>
   `,
   styles: []
 })
 export class TestComponent implements OnInit {
 
-  @Input() public parentData;
+  public name = "Jimmy";
+  public date = new Date();
 
-  @Output() public childEvent = new EventEmitter();
+  public person = {
+    "firstName": "John",
+    "lastName": "Doe"
+  }
 
   constructor() { }
 
   ngOnInit(): void {
-  }
-
-  fireEvent() {
-    this.childEvent.emit("Message from child");
   }
 }
