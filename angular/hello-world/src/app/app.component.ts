b7d6bd235361bcc0846c5c50d7297955defa86f9
trobertz <thomas.robertz@bcmsolutions.de>
2021-05-10 18:24:47 +0200

    Send from child to parent

--- angular/hello-world/src/app/app.component.ts
+++ angular/hello-world/src/app/app.component.ts
@@ -8,4 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'hello-world';
   public name = "From AppComponent";
+  public childMessage;
 }
--- angular/hello-world/src/app/test/test.component.ts
+++ angular/hello-world/src/app/test/test.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-test',
   template: `  
     <h2>{{ "This is Input from somewhere above: " + parentData }}</h2>
+    <button (click)="fireEvent()">Send to parent</button>
   `,
   styles: []
 })
@@ -11,8 +12,14 @@ export class TestComponent implements OnInit {
 
   @Input() public parentData;
 
+  @Output() public childEvent = new EventEmitter();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  fireEvent() {
+    this.childEvent.emit("Message from child");
   }
 }
