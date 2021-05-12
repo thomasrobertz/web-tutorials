a0a7a4a6a0decc740513e6d8f68418702ec3f80b
trobertz <thomas.robertz@bcmsolutions.de>
2021-05-12 17:36:13 +0200

    Wildcard and redirect

--- angular/hello-world-routing/src/app/department-list/department-list.component.ts
+++ angular/hello-world-routing/src/app/department-list/department-list.component.ts
@@ -3,14 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-department-list',
   template: `
-    <p>
-      department-list works!
-    </p>
+  <h2>Department List</h2>
+  <ul *ngFor="let department of departments">
+    <li>{{department.id}}, {{department.name}}</li>
+  </ul>
   `,
   styles: [
   ]
 })
 export class DepartmentListComponent implements OnInit {
+
+  departments = [
+    {"id": 1, "name": "Angular" },
+    {"id": 2, "name": "Node" },
+    {"id": 3, "name": "MongoDB" },
+    {"id": 4, "name": "Bootstrap" }
+  ];
 
   constructor() { }
 
--- angular/hello-world-routing/src/app/employee-list/employee-list.component.ts
+++ angular/hello-world-routing/src/app/employee-list/employee-list.component.ts
@@ -3,14 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-employee-list',
   template: `
-    <p>
-      employee-list works!
-    </p>
+  <h2>Employee List</h2>
+  <ul *ngFor="let employee of employees">
+    <li>{{employee.id}}, {{employee.name}}, {{employee.age}}</li>
+  </ul>
   `,
   styles: [
   ]
 })
 export class EmployeeListComponent implements OnInit {
+
+  employees = [
+    {"id": 1, "name": "John", "age": 34},
+    {"id": 2, "name": "Bob", "age": 35},
+    {"id": 3, "name": "Carl", "age": 38},
+    {"id": 4, "name": "Peter", "age": 27}
+  ];
 
   constructor() { }
 
