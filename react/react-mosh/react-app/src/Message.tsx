175dcc52c3bc44271d603f5b740a630f814b9ee8
Thomas Robertz <thomas.robertz@adesso.de>
2024-05-14 14:07:27 +0200

    Dynamic Content

--- react/react-mosh/react-app/src/Message.tsx
+++ react/react-mosh/react-app/src/Message.tsx
@@ -2,7 +2,10 @@
 function Message() {
     // Returning JSX (Javascript XML) - this is not HTML!
     // Will be transpiled to JS using babel (see babeljs.io/repl to try)
-    return <h1>Hello World</h1>
+
+    // Dynamic content is easy in JSX:
+    const name = "Thomas";
+    return <h1>Hello {name}</h1>
 }
 
 export default Message;
