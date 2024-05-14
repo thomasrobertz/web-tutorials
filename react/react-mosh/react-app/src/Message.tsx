3060cb4479a9943353ad1ca2be3c34ffe27c4168
Thomas Robertz <thomas.robertz@adesso.de>
2024-05-14 14:06:25 +0200

    HW Component

---
+++ react/react-mosh/react-app/src/Message.tsx
@@ -0,0 +1,8 @@
+// Pascal casing in React: Message instead of message
+function Message() {
+    // Returning JSX (Javascript XML) - this is not HTML!
+    // Will be transpiled to JS using babel (see babeljs.io/repl to try)
+    return <h1>Hello World</h1>
+}
+
+export default Message;
