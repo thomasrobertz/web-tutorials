9935d0394221ac9cba2eddfd69e8f16a12e98194
Thomas Robertz <thomas.robertz@adesso.de>
2024-05-15 12:51:44 +0200

    Dynamic data in component

--- react/react-mosh/react-app/src/components/ListGroup.tsx
+++ react/react-mosh/react-app/src/components/ListGroup.tsx
@@ -1,16 +1,22 @@
 import App from "../App";
 
 function ListGroup() {
+  const items = ["NYC", "Toronto", "Tokyo", "Paris", "Berlin"];
+
   return (
     <>
-        <h1>List</h1>
-        <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
-        </ul>
+      <h1>List</h1>
+      <ul className="list-group">
+        {
+          // In JSX use curly braces to break into JS
+          items.map((i, k) => (
+            /* The following is JSX again.
+                We also see how we give each element it's key.
+            */
+            <li key={k}>{i}</li>
+          ))
+        }
+      </ul>
     </>
   );
 }
