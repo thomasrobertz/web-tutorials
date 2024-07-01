0f52c151c3f305d6cac17bceba8e65f8e1b9275e
Thomas Robertz <thomas.robertz@adesso.de>
2024-07-01 13:47:08 +0200

    Initial commit

---
+++ spring/webflux/src/test/java/de/robertz/webflux/WebFluxDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.robertz.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebFluxDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
