b7608f2de7b7b88733a4ab2b419a2a869e048cd5
Thomas Robertz <thomas.robertz@adesso.de>
2024-08-20 16:28:35 +0200

    Add rows dynamically to table DOM

--- angular/bouali/angular-crash-course/src/app/my-first-comp/my-first-comp.component.ts
+++ angular/bouali/angular-crash-course/src/app/my-first-comp/my-first-comp.component.ts
@@ -9,6 +9,8 @@ export class MyFirstCompComponent {
   ts_name: string = "John";
   ts_email: string = "john@doe.com";
   ts_message: string = "Hello World";
+  ts_messages: Array<any> = [];
+
   isSubmitted: boolean = false;
 
   onSubmit() {
@@ -16,5 +18,10 @@ export class MyFirstCompComponent {
     console.log('Form submitted ' + this.ts_name 
       + " " + this.ts_email 
       + " " + this.ts_message);
+    this.ts_messages.push({
+      name: this.ts_name,
+      email: this.ts_email,
+      message: this.ts_message
+    });
   }
 }
